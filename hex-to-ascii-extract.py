f406c2b7df722a67d2314c48becfc24c1813aabd
Vishrant Vasavada <v.vasavada@hotmail.com>
2017-10-31 11:09:08 -0700

    adding simple hex to ascii parser

---
+++ hex-to-ascii-extract.py
@@ -0,0 +1,30 @@
+import string
+import os
+import sys
+import argparse
+
+def process_file(f_read, f_write):
+	
+	if not os.path.isfile(f_read):
+		print "No such file exists:", f_read
+
+	if os.path.isfile(f_write):
+		os.remove(f_write)
+		
+	with open(f_read, "r") as hex_istp:
+		hex_s = hex_istp.readline()
+		while len(hex_s) > 0:
+			printable = string.printable
+			translated_s = "".join(c if c in printable else '?' for c in hex_s)
+			print "Writing to file:", translated_s
+			with open(f_write, "a") as translated_istp:
+				translated_istp.write(translated_s)
+			hex_s = hex_istp.readline()
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="")
+	parser.add_argument("in_f", help="Input Hex ISTP File Path")
+	parser.add_argument("out_f", help="Output Translated File Path")
+	args = parser.parse_args()
+	
+	process_file(args.in_f, args.out_f)
